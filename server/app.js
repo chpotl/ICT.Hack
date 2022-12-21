45f1831dcaf4fc10fa5c7948a08512c2f68b1573
Leonid Zotov <zotovleonid@icloud.com>
2022-12-21 04:20:01 +0300

    build setup

--- server/app.js
+++ server/app.js
@@ -12,18 +12,38 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json({ limit: '10kb' }));
 app.use(globalErrorHandler);
-app.use(
-  cors({
-    origin: 'http://127.0.0.1:5173',
-    credentials: true,
-  })
-);
+if (process.env.NODE_ENV == 'prod') {
+  app.use(
+    cors({
+      origin: 'http://161.35.83.140:3030',
+      credentials: true,
+    })
+  );
+} else if (process.env.NODE_ENV == 'dev') {
+  app.use(
+    cors({
+      origin: 'http://127.0.0.1:5173',
+      credentials: true,
+    })
+  );
+}
 app.options('*', cors());
 
 if (process.env.NODE_ENV === 'dev') {
   app.use(morgan('dev'));
 }
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.use('/uploads', express.static(path.join(__dirname, './uploads')));
 app.use('/api', indexRouter);
+app.get('/*', function (req, res) {
+  res.sendFile(
+    path.join(__dirname, '../client/build/index.html'),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
 
 module.exports = app;
