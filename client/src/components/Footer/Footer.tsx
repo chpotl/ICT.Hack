abaa9fcdda6e07f2cc9984f939a528cc6dee0a8a
Vasily <v.smirnov@niuitmo.ru>
2022-12-21 20:21:58 +0300

    rename footer

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react"
 export const Footer = () => {
   return (
     <footer className='p-5 flex justify-center text-darkGreen w-full'>
-      &copy; smartkontora aka no brain solutions
+      &copy; Цифровая Платформа ИТМО x ПИШ 2022
     </footer>
   )
 }
